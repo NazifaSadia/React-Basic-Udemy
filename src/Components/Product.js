08009b0458f811ce14f32f19901cb2fa24cdd4a6
NazifaSadia <nazifas323@gmail.com>
2020-07-09 13:41:01 +0600

    Working with Property - Function

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -6,10 +6,13 @@ const Product = (props) => {
 
     const buy = ()=> setCount(count+1);
 
+    // const show = ()=> props.handleShow(props.name);
+    
     return (
         <div>
             <p>{props.name} - ${props.price}</p>
             <button onClick={buy}>Buy</button>
+            <button onClick={()=> props.handleShow(props.name)}>Show</button>
             <h3>Quantity: {count} items</h3>
             <hr/>
         </div>
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -3,11 +3,16 @@ import Product from './Product';
 import Total from './Total';
 
 const ProductList = () => {
+
+    const showProduct = (name)=> {
+        alert("You selected " + name);
+    } 
+
     return (
         <div>
-            <Product name="Android" price="121"/>
-            <Product name="Apple" price="123"/>
-            <Product name="Nokia" price="65"/>
+            <Product name="Android" price="121" handleShow={showProduct}/>
+            <Product name="Apple" price="123" handleShow={showProduct}/>
+            <Product name="Nokia" price="65" handleShow={showProduct}/>
             <Total/>
         </div>
     );
